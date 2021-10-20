9d3d51dd700e431849a76d9d64579a27b8f198c1
Steven Chen <steven.chen.121@gmail.com>
2021-10-20 18:33:11 -0400

    Added typewriter functionality and additional stylings.

--- script.js
+++ script.js
@@ -4,31 +4,59 @@
 
 const app = {};
 
-app.stringsToType = ["Steven Chen", "a Freelancer", "a Web-Developer"];
+// Typewriter variables
+app.strings = ["I am a learner", "I am a movie nerd", "I am Dark Mode", "I am a Web Developer", "I'm Steven Chen"];
+app.sIndex = 0;
+app.textPosition = 0;
+app.speed = 75;
+app.h1 = document.querySelector('.typewriter');
 
 app.init = () => {
-    // do some shit
-    // app.stringsToType.forEach((stringToType) => {
-    //     app.typewriter(stringToType);
-    // });
-    app.navButtonListener();
-    // app.projectModal();
+
+    app.addWindowEventListeners();
     app.skillContainerListener();
+    app.toggleEventListener();
 };
 
-app.navButtonListener = () => {
-    const toTop = document.querySelector('.to-top');
-    
-    window.addEventListener('scroll', () => {
-        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-            toTop.classList.remove('hidden');
-        } else {
-            toTop.classList.add('hidden');
-        }
+// ===========================================
+// TYPEWRITER functions
+// ===========================================
+
+app.typewriter = () => {
+    app.h1.innerHTML = app.strings[app.sIndex].substring(0, app.textPosition) + '<span class="blinker">&#124</span>';
+    if (app.textPosition++ != app.strings[app.sIndex].length) {
+        setTimeout(app.typewriter, app.speed);
+    }
+}
+
+// Async await only works with promises so we make a new promise
+app.typeDelay = (ms) => {
+    return new Promise((res) => {
+        setTimeout(res, ms);
     });
-};
+}
 
-app.skillContainerListener = () =>{
+// Create the loop of writing out the phrases continuously
+app.introText = async () => {
+    for (let i = 0; i < app.strings.length; i++) {
+        app.typewriter();
+        await app.typeDelay(app.strings[i].length * app.speed + 1500);
+        app.textPosition = 0;
+        ++app.sIndex;
+    }
+
+    // to loop infinitely
+    await app.typeDelay(5000);
+    app.sIndex = 0;
+    app.introText();
+}
+
+// ===========================================
+// end of Typewriter functions
+// ===========================================
+
+
+app.skillContainerListener = () => {
     const skills = document.querySelectorAll('.skill-container');
 
     skills.forEach((skill) => {
@@ -41,38 +69,41 @@ app.skillContainerListener = () =>{
     });
 };
 
+app.addWindowEventListeners = () => {
+    const toTop = document.querySelector('.to-top');
 
-// app.projectModal = () => {
-//     const projectButton = document.querySelector('.project-button');
-//     const modal = document.querySelector('.modal');
-//     const modalClose = document.querySelector('.close-modal');
-//     const body = document.querySelector('body');
+    window.addEventListener('scroll', () => {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            toTop.classList.remove('hidden');
+        } else {
+            toTop.classList.add('hidden');
+        }
+    });
 
-//     projectButton.addEventListener('click', () => {
-//         // body.classList.add('modal-open');
-//         modal.classList.remove('hidden');
-//     });
+    window.addEventListener("load", app.introText);
+};
 
-//     modalClose.addEventListener('click', () => {
-//         // body.classList.remove('modal-open');
-//         modal.classList.add('hidden');
-//     });
-// };
+app.toggleDarkMode = () => {
+    const dark = document.querySelector('.fa-moon');
+    const light = document.querySelector('.fa-sun');
 
+    if (dark.classList.contains('chosen')) {
+        // dark.classList.remove('chosen');
+        // do some light mode shit
+    } else {
+        // toggle.classList.add('chosen');
+        // do some dark mode shit
+    }
 
+    dark.classList.toggle('chosen');
+    light.classList.toggle('chosen');
 
-// app.typewriter = (toType) => {
-//     const stringArray = [...toType];
+};
 
-//     console.log('test');
-//     // stringArray.forEach((char)=>{
-//     //     setTimeout(()=>{
-//     //         typewriterSpan.innerText = (typewriterSpan.innerText !== null ? typewriterSpan.innerText : '') + char;
-//     //     }, 500);
-//     // })
-//     // console.log(wordArray);
-// };
-
+app.toggleEventListener = () => {
+    const darkToggle = document.querySelector('.toggle-dark');
+    darkToggle.addEventListener('click', app.toggleDarkMode);
+}
 
 
 app.init();
